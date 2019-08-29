678c63d4edd2aa5449c2a09c4b77fd0d294dc7bf
Cankat Saraç <cankat.sarac@stu.bahcesehir.edu.tr>
2019-08-29 12:23:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,5 @@
-# code your #position_taken? method here!def position_taken?(board, index)
+# code your #position_taken? method here!
+def position_taken?(board, index)
   result = nil
   if board[index] == "" ||
     board[index] == " " ||
